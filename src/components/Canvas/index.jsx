a1e04d7817b33ed3c9f4946e1ddb6952a6264198
leo <leonardoandrei41@gmail.com>
2024-03-01 13:43:21 -0300

    Add a border of 1px to the Canvas

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './canvas.module.css';
 
 export default function Canvas({ width, height, pixelSize }) {
   return (
-    <div>
-      <canvas width={width * pixelSize} height={height * pixelSize} />
-    </div>
+    <canvas className={styles.canvas} width={width * pixelSize} height={height * pixelSize} />
   );
 }
 
