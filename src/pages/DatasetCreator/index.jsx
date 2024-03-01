4bb8c836124cf02031f4d15039978bf081d4c8c8
leo <leonardoandrei41@gmail.com>
2024-03-01 18:48:00 -0300

    Implement list of unique labels in DatasetCreator

--- src/pages/DatasetCreator/index.jsx
+++ src/pages/DatasetCreator/index.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './dataset-creator.module.css';
 
 export default function DatasetCreator() {
-  return (<div />);
+  const [labels, setLabels] = useState([]);
+  const [labelInput, setLabelInput] = useState('');
+
+  const addLabel = () => {
+    if (!labelInput || labels.includes(labelInput)) {
+      return;
+    }
+    setLabels((prevLabels) => [...prevLabels, labelInput.toString()]);
+    setLabelInput('');
+  };
+
+  const removeLabel = (labelToRemove) => {
+    if (!labels.includes(labelToRemove)) {
+      return;
+    }
+    setLabels((prevLabels) => prevLabels.filter((label) => label !== labelToRemove));
+  };
+
+  const onInputLabelKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      addLabel();
+    }
+  };
+
+  return (
+    <div>
+      <div id={styles['label-list']}>
+        {
+          labels.map((label) => (
+            <div key={label}>
+              <p>{label}</p>
+              <button type="button" onClick={() => removeLabel(label)}>x</button>
+            </div>
+          ))
+        }
+      </div>
+      <input
+        placeholder="New label"
+        value={labelInput}
+        onChange={({ target }) => setLabelInput(target.value)}
+        onKeyDown={onInputLabelKeyDown}
+      />
+    </div>
+  );
 }
