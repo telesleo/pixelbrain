3b437f4c40dc4d543855ee22713da8c5ce78c2c5
leo <leonardoandrei41@gmail.com>
2024-03-01 14:02:59 -0300

    Implement display of values in Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Canvas from './components/Canvas';
 
 const CANVAS_SIZE = 16;
 const PIXEL_SIZE = 10;
 
 function App() {
+  const [canvasValues] = useState(Array(CANVAS_SIZE ** 2).fill(0));
+
   return (
     <div className="App">
-      <Canvas width={CANVAS_SIZE} height={CANVAS_SIZE} pixelSize={PIXEL_SIZE} />
+      <Canvas
+        values={canvasValues}
+        width={CANVAS_SIZE}
+        height={CANVAS_SIZE}
+        pixelSize={PIXEL_SIZE}
+      />
     </div>
   );
 }
--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './canvas.module.css';
 
-export default function Canvas({ width, height, pixelSize }) {
+export default function Canvas({
+  values, width, height, pixelSize,
+}) {
+  const canvasRef = useRef();
+
+  const [context, setContext] = useState();
+
+  useEffect(() => {
+    setContext(canvasRef.current.getContext('2d'));
+  }, [canvasRef]);
+
+  useEffect(() => {
+    if (!context) return;
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    context.fillStyle = 'black';
+    for (let index = 0; index < values.length; index += 1) {
+      if (values[index] > 0) {
+        const x = index % width;
+        const y = Math.floor(index / width);
+        context.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+      }
+    }
+  }, [context, values]);
+
   return (
-    <canvas className={styles.canvas} width={width * pixelSize} height={height * pixelSize} />
+    <canvas
+      className={styles.canvas}
+      ref={canvasRef}
+      width={width * pixelSize}
+      height={height * pixelSize}
+    />
   );
 }
 
 Canvas.propTypes = {
+  values: PropTypes.arrayOf(PropTypes.number).isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   pixelSize: PropTypes.number.isRequired,
